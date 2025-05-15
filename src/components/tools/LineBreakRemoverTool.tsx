5201f89613da3edced7917d07246bc52eaa2aa77
Wikipedia <wikipediasecond05@gmail.com>
2025-05-15 09:21:46 +0000

    The tool Line Break Remover is no opening, there is some error.

--- src/components/tools/LineBreakRemoverTool.tsx
+++ src/components/tools/LineBreakRemoverTool.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import * as React from 'react';
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
